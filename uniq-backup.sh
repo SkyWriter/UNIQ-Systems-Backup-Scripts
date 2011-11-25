7e617fea9717db4e145299e1e7642f51516cc56a
Ivan Kasatenko <sky.31338@gmail.com>
2011-11-25 11:20:55 +0400

    Added log exclusion

--- uniq-backup.sh
+++ uniq-backup.sh
@@ -9,4 +9,4 @@ TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
 BAKFILE=$BACKUPDIR/$BASENAME-$TIMESTAMP.dat
 SNARFILE=$BACKUPDIR/$BASENAME.snar
 
-nice ionice -c 3 tar --create --verbose --listed-incremental=$SNARFILE $SOURCEDIR | bzip2 | openssl enc -e -aes-256-cbc -pass pass:$PASSWORD > $BAKFILE
+nice ionice -c 3 tar --create --verbose --exclude '*.log' --listed-incremental=$SNARFILE $SOURCEDIR | bzip2 | openssl enc -e -aes-256-cbc -pass pass:$PASSWORD > $BAKFILE
